b7b1f8f30dcb754f31e52285275ca1111b42254c
AssassinTed <theodorfbi@gmail.com>
2017-01-19 20:11:26 +0200

    Add files via upload

---
+++ FirstWPF/FirstWPF/MainWindow.xaml.vb
@@ -0,0 +1,33 @@
+﻿
+
+
+Class MainWindow
+
+    Private Sub textBox_KeyDown(sender As Object, e As KeyEventArgs) Handles textBox.KeyDown
+
+
+    End Sub
+
+
+
+    Private Sub textBox_TextChanged(sender As Object, e As TextChangedEventArgs) Handles textBox.TextChanged
+        Dim cursorPos = textBox.SelectionStart
+        Dim input = textBox.Text
+        Dim output = ""
+        For i = 0 To input.Length - 1
+            If "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ".IndexOf(input.Chars(i)) >= 0 Then
+                output &= input.Chars(i)
+            ElseIf "αβγδεζηθικλμνξοπρστυφχψω".IndexOf(input.Chars(i)) >= 0 Then
+
+                output &= StrConv(input.Chars(i), VbStrConv.Uppercase)
+
+
+
+            Else
+                cursorPos -= 1
+            End If
+        Next
+        textBox.Text = output
+        textBox.SelectionStart = cursorPos
+    End Sub
+End Class
